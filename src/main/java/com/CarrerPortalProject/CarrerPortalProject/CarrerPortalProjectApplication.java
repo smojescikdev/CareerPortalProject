e07acf6ccd91b5c77b507b532e51ad961efbfe69
smojescikdev <s.mojescik.dev@gmail.com>
2024-06-15 19:47:42 +0200

    Initial commit

---
+++ src/main/java/com/CarrerPortalProject/CarrerPortalProject/CarrerPortalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.CarrerPortalProject.CarrerPortalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrerPortalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrerPortalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CarrerPortalProject/CarrerPortalProject/CarrerPortalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CarrerPortalProject.CarrerPortalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrerPortalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
