af3f917bef08479e6a8939c2ad043b50a18f11cf
smojescikdev <s.mojescik.dev@gmail.com>
2024-06-30 08:34:28 +0200

    refactoring & added jpql in JobPostActivityRepository

--- src/main/java/com/CarrerPortalProject/CarrerPortalProject/repository/JobPostActivityRepository.java
+++ src/main/java/com/CarrerPortalProject/CarrerPortalProject/repository/JobPostActivityRepository.java
@@ -2,6 +2,7 @@ package com.CarrerPortalProject.CarrerPortalProject.repository;
 
 import com.CarrerPortalProject.CarrerPortalProject.model.JobPostActivity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +12,12 @@ public interface JobPostActivityRepository extends JpaRepository<JobPostActivity
 
     List<JobPostActivity> findByPostedById_UserId(int postedById);
 
+
     // Metoda zwracająca listę ofert pracy posortowaną od najnowszych do najstarszych
+    //List<JobPostActivity> findAllByOrderByPostedDateDesc();
+
+
+    //lista ofert pracy -- DESC -- JPQL
+    @Query("SELECT j FROM JobPostActivity j ORDER BY j.postedDate DESC")
     List<JobPostActivity> findAllByOrderByPostedDateDesc();
 }
